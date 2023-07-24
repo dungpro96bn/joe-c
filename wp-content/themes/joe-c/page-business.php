859d069c159a20654f66ac92a01f1c1e1c4a75b8
Dũng Bùi <dungpro96bn@gmail.com>
2023-07-24 14:17:06 +0700

    fix fadeIn

--- wp-content/themes/joe-c/page-business.php
+++ wp-content/themes/joe-c/page-business.php
@@ -38,7 +38,7 @@
         <div class="business-main page-content">
 
             <div class="business-tabs">
-                <div class="business-tab">
+                <div class="business-tab u-inview">
                     <a class="scroll" href="#business-01">
                         <picture>
                             <source srcset="/wp-content/uploads/business_pic01_pc.png">
@@ -53,7 +53,7 @@
                         </div>
                     </a>
                 </div>
-                <div class="business-tab">
+                <div class="business-tab u-inview">
                     <a class="scroll" href="#business-02">
                         <picture>
                             <source srcset="/wp-content/uploads/business_pic02_pc.png">
@@ -72,14 +72,14 @@
 
             <div class="description">
                 <div class="inner">
-                    <h2 class="title">JOEの２つの事業</h2>
-                    <p class="text">人、コト、情報をつなぎ、世界をもっと面白く。<br/>国境を越えた雇用の創出、<br class="sp-br"/>HR Techを活用した人材育成と海外人材支援など、<br/>様々な角度から社会および国際貢献を<br class="sp-br"/>行ってまいります。</p>
+                    <h2 class="title u-inview">JOEの２つの事業</h2>
+                    <p class="text u-inview">人、コト、情報をつなぎ、世界をもっと面白く。<br/>国境を越えた雇用の創出、<br class="sp-br"/>HR Techを活用した人材育成と海外人材支援など、<br/>様々な角度から社会および国際貢献を<br class="sp-br"/>行ってまいります。</p>
                 </div>
             </div>
 
             <div id="business-01" class="business-content">
                 <div class="banner">
-                    <div class="image">
+                    <div class="image u-inview">
                         <picture>
                             <source media="(max-width: 767px)" srcset="/wp-content/uploads/business_pic03_sp.png">
                             <source media="(min-width: 768px)" srcset="/wp-content/uploads/business_pic03_pc.png">
@@ -89,7 +89,7 @@
                     <div class="banner-info">
                         <div class="inner">
                             <div class="info-inner">
-                                <div class="text-info">
+                                <div class="text-info u-inview">
                                     <p class="number en">01</p>
                                     <p class="title">JOEの技能実習事業</p>
                                     <p class="text">外国人技能実習制度とは、国際社会と調和ある発展を見込み、日本で培われた技能や技術等を開発途上地域へ移転を図り、当該地域の経済発展を担う『人づくり』に寄与することを目的として創設された制度です。</p>
@@ -101,13 +101,13 @@
                 </div>
                 <div class="flow">
                     <div class="inner">
-                        <h2 class="heading-block">
+                        <h2 class="heading-block u-inview">
                             <span class="ttl-en">FLOW</span>
                             <span class="ttl-ja">技能実習生受入の流れ</span>
                         </h2>
                         <div class="flow-content">
                             <ul class="flow-list">
-                                <li class="flow-item">
+                                <li class="flow-item u-inview">
                                     <div class="number en"><p>01</p></div>
                                     <div class="flow-info">
                                         <p class="ttl"><span class="number-sp en">01</span>お申し込み・求人開始</p>
@@ -124,7 +124,7 @@
                                         </picture>
                                     </div>
                                 </li>
-                                <li class="flow-item">
+                                <li class="flow-item u-inview">
                                     <div class="number en"><p>02</p></div>
                                     <div class="flow-info">
                                         <p class="ttl"><span class="number-sp en">02</span>現地面接・選考、採用</p>
@@ -141,7 +141,7 @@
                                         </picture>
                                     </div>
                                 </li>
-                                <li class="flow-item">
+                                <li class="flow-item u-inview">
                                     <div class="number en"><p>03</p></div>
                                     <div class="flow-info">
                                         <p class="ttl"><span class="number-sp en">03</span>現地研修(4～6ヶ月間)</p>
@@ -158,7 +158,7 @@
                                         </picture>
                                     </div>
                                 </li>
-                                <li class="flow-item">
+                                <li class="flow-item u-inview">
                                     <div class="number en"><p>04</p></div>
                                     <div class="flow-info">
                                         <p class="ttl"><span class="number-sp en">04</span>入管申請・入国、日本語研修</p>
@@ -181,13 +181,13 @@
                 </div>
                 <div class="support">
                     <div class="inner">
-                        <h2 class="heading-block">
+                        <h2 class="heading-block u-inview">
                             <span class="ttl-en">SUPPORT</span>
                             <span class="ttl-ja">サポート体制</span>
                         </h2>
-                        <p class="text-description">JOE協同組合では、様々なサポートプランをご用意しております。<br/>監理団体組合が本来やるべき項目のほか、多彩なサポートをアレンジ。<br/>提携会社によるリモートコンテンツの提供も豊富で、企業様のご要望をしっかりバックアップいたします。</p>
+                        <p class="text-description u-inview">JOE協同組合では、様々なサポートプランをご用意しております。<br/>監理団体組合が本来やるべき項目のほか、多彩なサポートをアレンジ。<br/>提携会社によるリモートコンテンツの提供も豊富で、企業様のご要望をしっかりバックアップいたします。</p>
                         <div class="support-content">
-                            <div class="support-title">
+                            <div class="support-title u-inview">
                                 <p class="number-ttl en">01</p>
                                 <div class="info">
                                     <p class="ttl">日本語能力・生産性をレベルアップ</p>
@@ -195,8 +195,8 @@
                                 </div>
                             </div>
                             <ul class="support-list">
-                                <li class="item-sp"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
-                                <li class="support-item">
+                                <li class="item-sp u-inview"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
+                                <li class="support-item u-inview">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon01_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon01_pc.png" alt="">
@@ -212,7 +212,7 @@
                                         <p class="text">豊富な日本語学習動画教材を提供。各教育機関等と共に、初歩的なN4レベルからN1を目指すハイレベルまで、しっかりサポートします。</p>
                                     </div>
                                 </li>
-                                <li class="support-item">
+                                <li class="support-item u-inview">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon02_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon02_pc.png" alt="">
@@ -231,7 +231,7 @@
                             </ul>
                         </div>
                         <div class="support-content">
-                            <div class="support-title support-title-02">
+                            <div class="support-title support-title-02 u-inview">
                                 <p class="number-ttl en">02</p>
                                 <div class="info">
                                     <p class="ttl">その他、幅広くサポートいたします</p>
@@ -239,8 +239,8 @@
                                 </div>
                             </div>
                             <ul class="support-list">
-                                <li class="item-sp"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
-                                <li class="support-item">
+                                <li class="item-sp u-inview"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
+                                <li class="support-item u-inview">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon03_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon03_pc.png" alt="">
@@ -256,7 +256,7 @@
                                         <p class="text">配属前に必要な社宅や生活備品、インターネット環境などの準備、その他生活環境の整備についてバックアップします。</p>
                                     </div>
                                 </li>
-                                <li class="support-item">
+                                <li class="support-item u-inview">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon04_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon04_pc.png" alt="">
@@ -272,7 +272,7 @@
                                         <p class="text">毎月1回、社宅を訪問し、チェックシートに基づいて利用状況を確認してご報告いたします。また、必要に応じて指導・面談を実施します。</p>
                                     </div>
                                 </li>
-                                <li class="support-item">
+                                <li class="support-item u-inview">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon05_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon05_pc.png" alt="">
@@ -296,7 +296,7 @@
 
             <div id="business-02" class="business-content">
                 <div class="banner">
-                    <div class="image">
+                    <div class="image u-inview">
                         <picture>
                             <source media="(max-width: 767px)" srcset="/wp-content/uploads/business_pic08_sp.png">
                             <source media="(min-width: 768px)" srcset="/wp-content/uploads/business_pic08_pc.png">
@@ -306,7 +306,7 @@
                     <div class="banner-info">
                         <div class="inner">
                             <div class="info-inner">
-                                <div class="text-info">
+                                <div class="text-info u-inview">
                                     <p class="number en">02</p>
                                     <p class="title">共同購買事業</p>
                                     <p class="text">組合員様同士が互いに協力し、相互扶助の精神に基づいて、経営の合理化や経済的地位の向上・改善を図るための事業を行います。その他、組合員様の事業に関する経営および技術の向上または知識を深めるための教育や情報の提供なども行います。</p>
@@ -318,7 +318,7 @@
                 </div>
 
                 <div class="inner">
-                    <div class="business-map">
+                    <div class="business-map u-inview">
                         <picture>
                             <source media="(max-width: 767px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/business_map_sp.svg">
                             <source media="(min-width: 768px)" srcset="<?php bloginfo('template_directory'); ?>/assets/images/business_map_pc.svg">
@@ -331,21 +331,21 @@
                     <div class="business-point point-01">
                         <div class="text-content">
                             <div class="text-inner">
-                                <div class="point-title">
+                                <div class="point-title u-inview">
                                     <p class="ttl-en">
                                         <span class="t1">POINT</span>
                                         <span class="t2">01</span>
                                     </p>
                                     <p class="ttl">新電力の<br class="sp-br"/>共同活用購買</p>
                                 </div>
-                                <div class="text-description">
+                                <div class="text-description u-inview">
                                     <p class="text">2016年4月より始まった電力の全面自由化に伴い、<br class="pc-br"/>電力を利用しているすべての方が自由化の対象となりました。<br/>
                                         JOE協同組合では、組合員企業様の電気代削減の支援を行い、多数の削減例をもとに<br/>
                                         経験豊富なコンサルタントが、一切のお手続きまでをサポートいたします。<br/>
                                         公平中立な立場で、最適のプランをご提案。ご利用やご相談はもちろん無料です。</p>
                                 </div>
                                 <ul class="point-infoList">
-                                    <li class="point-infoItem">
+                                    <li class="point-infoItem u-inview">
                                         <picture class="icon">
                                             <source srcset="/wp-content/uploads/business_icon06_pc.png">
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon06_pc.png" alt="">
@@ -361,7 +361,7 @@
                                             <p class="text">北海道から九州まで、幅広く対応いたします。豊富なご提案から、納得のプランをお選びいただけます。</p>
                                         </div>
                                     </li>
-                                    <li class="point-infoItem">
+                                    <li class="point-infoItem u-inview">
                                         <picture class="icon">
                                             <source srcset="/wp-content/uploads/business_icon07_pc.png">
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon07_pc.png" alt="">
@@ -377,7 +377,7 @@
                                             <p class="text">公平中立な立場で、企業様の条件に最適な電力会社を<br class="pc-br"/>ご提案します。</p>
                                         </div>
                                     </li>
-                                    <li class="point-infoItem">
+                                    <li class="point-infoItem u-inview">
                                         <picture class="icon">
                                             <source srcset="/wp-content/uploads/business_icon08_pc.png">
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon08_pc.png" alt="">
@@ -396,7 +396,7 @@
                                 </ul>
                             </div>
                         </div>
-                        <div class="image-content">
+                        <div class="image-content u-inview">
                             <picture>
                                 <source media="(max-width: 767px)" srcset="/wp-content/uploads/business_pic09_sp.png">
                                 <source media="(min-width: 768px)" srcset="/wp-content/uploads/business_pic09_pc.png">
@@ -405,7 +405,7 @@
                         </div>
                     </div>
                     <div class="business-point point-02">
-                        <div class="image-content">
+                        <div class="image-content u-inview">
                             <picture>
                                 <source media="(max-width: 767px)" srcset="/wp-content/uploads/business_pic10_sp.png">
                                 <source media="(min-width: 768px)" srcset="/wp-content/uploads/business_pic10_pc.png">
@@ -414,7 +414,7 @@
                         </div>
                         <div class="text-content">
                             <div class="text-inner">
-                                <div class="point-title">
+                                <div class="point-title u-inview">
                                     <p class="ttl-en">
                                         <span class="t1">POINT</span>
                                         <span class="t2">02</span>
@@ -422,11 +422,11 @@
                                     <p class="ttl">組合員のための<br/>消耗品等の共同購買</p>
                                 </div>
                                 <div class="text-description">
-                                    <p class="text">組合員企業様のコスト削減に向けて、<br/>
+                                    <p class="text u-inview">組合員企業様のコスト削減に向けて、<br/>
                                         多数の消耗品業者と提携することにより、<br/>
                                         組合員の皆様へ特別価格で提供いたします。
                                     </p>
-                                    <p class="text">各資材、事務用品等ご用意しておりますので、<br/>
+                                    <p class="text u-inview">各資材、事務用品等ご用意しておりますので、<br/>
                                         お気軽にお問い合わせください。<br/>
                                         また、共同購入商品やサービスのご希望等がございましたら、<br/>
                                         ぜひご提案もお待ちしております。</p>
