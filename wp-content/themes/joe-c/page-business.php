1498900cd27f496bb6b7036cbd8308f3f7b0b473
Dũng Bùi <dungpro96bn@gmail.com>
2023-07-24 14:07:19 +0700

    Style business page

--- wp-content/themes/joe-c/page-business.php
+++ wp-content/themes/joe-c/page-business.php
@@ -10,6 +10,22 @@
             </picture>
         </div>
 
+        <div class="bg-02">
+            <picture>
+                <source media="(max-width: 767px)" srcset="/wp-content/uploads/home_bg03_sp.png">
+                <source media="(min-width: 768px)" srcset="/wp-content/uploads/home_bg03_pc.png">
+                <img decoding="async" class="sizes" src="/wp-content/uploads/home_bg03_pc.png" alt="">
+            </picture>
+        </div>
+
+        <div class="bg-03">
+            <picture>
+                <source media="(max-width: 767px)" srcset="/wp-content/uploads/business_bg03_sp.png">
+                <source media="(min-width: 768px)" srcset="/wp-content/uploads/business_bg03_pc.png">
+                <img decoding="async" class="sizes" src="/wp-content/uploads/business_bg03_pc.png" alt="">
+            </picture>
+        </div>
+
         <div class="header-entry">
             <div class="inner">
                 <h1 class="heading u-inview">
@@ -57,7 +73,7 @@
             <div class="description">
                 <div class="inner">
                     <h2 class="title">JOEの２つの事業</h2>
-                    <p class="text">人、コト、情報をつなぎ、世界をもっと面白く。<br/>国境を越えた雇用の創出、HR Techを活用した人材育成と海外人材支援など、<br/>様々な角度から社会および国際貢献を行ってまいります。</p>
+                    <p class="text">人、コト、情報をつなぎ、世界をもっと面白く。<br/>国境を越えた雇用の創出、<br class="sp-br"/>HR Techを活用した人材育成と海外人材支援など、<br/>様々な角度から社会および国際貢献を<br class="sp-br"/>行ってまいります。</p>
                 </div>
             </div>
 
@@ -179,13 +195,20 @@
                                 </div>
                             </div>
                             <ul class="support-list">
+                                <li class="item-sp"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
                                 <li class="support-item">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon01_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon01_pc.png" alt="">
                                     </picture>
                                     <div class="info">
-                                        <p class="title">リモート学習教材の共同開発</p>
+                                        <div class="title">
+                                            <picture class="icon-sp">
+                                                <source srcset="/wp-content/uploads/business_icon01_pc.png">
+                                                <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon01_pc.png" alt="">
+                                            </picture>
+                                            <div class="sub-text">リモート学習教材の<br class="sp-br"/>共同開発</div>
+                                        </div>
                                         <p class="text">豊富な日本語学習動画教材を提供。各教育機関等と共に、初歩的なN4レベルからN1を目指すハイレベルまで、しっかりサポートします。</p>
                                     </div>
                                 </li>
@@ -195,14 +218,20 @@
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon02_pc.png" alt="">
                                     </picture>
                                     <div class="info">
-                                        <p class="title">独自の評価・教育ツール</p>
+                                        <div class="title">
+                                            <picture class="icon-sp">
+                                                <source srcset="/wp-content/uploads/business_icon02_pc.png">
+                                                <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon02_pc.png" alt="">
+                                            </picture>
+                                            <div class="sub-text">独自の評価・教育ツール</div>
+                                        </div>
                                         <p class="text">日常行動として求められることを実践的にトレーニングすることで、個々の行動レベルの向上・意識改革・Employability（勤労資質）向上に繋げるプログラムです。</p>
                                     </div>
                                 </li>
                             </ul>
                         </div>
                         <div class="support-content">
-                            <div class="support-title">
+                            <div class="support-title support-title-02">
                                 <p class="number-ttl en">02</p>
                                 <div class="info">
                                     <p class="ttl">その他、幅広くサポートいたします</p>
@@ -210,13 +239,20 @@
                                 </div>
                             </div>
                             <ul class="support-list">
+                                <li class="item-sp"><p>日本語の自主学習ツール、ならびに日本で仕事をするうえで必要な基本的資質を向上させるプログラムを提供します。</p></li>
                                 <li class="support-item">
                                     <picture class="icon">
                                         <source srcset="/wp-content/uploads/business_icon03_pc.png">
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon03_pc.png" alt="">
                                     </picture>
                                     <div class="info">
-                                        <p class="title">インフラ代行<small>面倒な業務をスリム化するお手伝い</small></p>
+                                        <div class="title">
+                                            <picture class="icon-sp">
+                                                <source srcset="/wp-content/uploads/business_icon03_pc.png">
+                                                <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon03_pc.png" alt="">
+                                            </picture>
+                                            <div class="sub-text">インフラ代行<small>面倒な業務をスリム化するお手伝い</small></div>
+                                        </div>
                                         <p class="text">配属前に必要な社宅や生活備品、インターネット環境などの準備、その他生活環境の整備についてバックアップします。</p>
                                     </div>
                                 </li>
@@ -226,7 +262,13 @@
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon04_pc.png" alt="">
                                     </picture>
                                     <div class="info">
-                                        <p class="title">社宅巡回<small>社宅の衛生管理と失踪リスク軽減に</small></p>
+                                        <div class="title">
+                                            <picture class="icon-sp">
+                                                <source srcset="/wp-content/uploads/business_icon04_pc.png">
+                                                <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon04_pc.png" alt="">
+                                            </picture>
+                                            <div class="sub-text">社宅巡回<small>社宅の衛生管理と失踪リスク軽減に</small></div>
+                                        </div>
                                         <p class="text">毎月1回、社宅を訪問し、チェックシートに基づいて利用状況を確認してご報告いたします。また、必要に応じて指導・面談を実施します。</p>
                                     </div>
                                 </li>
@@ -236,7 +278,13 @@
                                         <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon05_pc.png" alt="">
                                     </picture>
                                     <div class="info">
-                                        <p class="title">24hコンシェル<small>帰国までの生活サポートをおまかせ</small></p>
+                                        <div class="title">
+                                            <picture class="icon-sp">
+                                                <source srcset="/wp-content/uploads/business_icon05_pc.png">
+                                                <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon05_pc.png" alt="">
+                                            </picture>
+                                            <div class="sub-text">24hコンシェル<small>帰国までの生活サポートをおまかせ</small></div>
+                                        </div>
                                         <p class="text">24時間365日対応のコールセンターと連動したサポート体制を確立。配属前のオリエンテーションから帰国時の送迎まで生活サポートの安心をお届けします。</p>
                                     </div>
                                 </li>
@@ -288,11 +336,10 @@
                                         <span class="t1">POINT</span>
                                         <span class="t2">01</span>
                                     </p>
-                                    <p class="ttl">新電力の共同活用購買</p>
+                                    <p class="ttl">新電力の<br class="sp-br"/>共同活用購買</p>
                                 </div>
                                 <div class="text-description">
-                                    <p class="text">2016年4月より始まった電力の全面自由化に伴い、<br/>
-                                        電力を利用しているすべての方が自由化の対象となりました。<br/>
+                                    <p class="text">2016年4月より始まった電力の全面自由化に伴い、<br class="pc-br"/>電力を利用しているすべての方が自由化の対象となりました。<br/>
                                         JOE協同組合では、組合員企業様の電気代削減の支援を行い、多数の削減例をもとに<br/>
                                         経験豊富なコンサルタントが、一切のお手続きまでをサポートいたします。<br/>
                                         公平中立な立場で、最適のプランをご提案。ご利用やご相談はもちろん無料です。</p>
@@ -304,7 +351,13 @@
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon06_pc.png" alt="">
                                         </picture>
                                         <div class="info">
-                                            <p class="title">全国をカバー</p>
+                                            <div class="title">
+                                                <picture class="icon-sp">
+                                                    <source srcset="/wp-content/uploads/business_icon06_pc.png">
+                                                    <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon06_pc.png" alt="">
+                                                </picture>
+                                                <div class="sub-text">全国をカバー</div>
+                                            </div>
                                             <p class="text">北海道から九州まで、幅広く対応いたします。豊富なご提案から、納得のプランをお選びいただけます。</p>
                                         </div>
                                     </li>
@@ -314,8 +367,14 @@
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon07_pc.png" alt="">
                                         </picture>
                                         <div class="info">
-                                            <p class="title">一括見積もり・ご相談無料</p>
-                                            <p class="text">公平中立な立場で、企業様の条件に最適な電力会社を<br/>ご提案します。</p>
+                                            <div class="title">
+                                                <picture class="icon-sp">
+                                                    <source srcset="/wp-content/uploads/business_icon07_pc.png">
+                                                    <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon07_pc.png" alt="">
+                                                </picture>
+                                                <div class="sub-text">一括見積もり・<br class="sp-br"/>ご相談無料</div>
+                                            </div>
+                                            <p class="text">公平中立な立場で、企業様の条件に最適な電力会社を<br class="pc-br"/>ご提案します。</p>
                                         </div>
                                     </li>
                                     <li class="point-infoItem">
@@ -324,7 +383,13 @@
                                             <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon08_pc.png" alt="">
                                         </picture>
                                         <div class="info">
-                                            <p class="title">面倒な手続きもサポートで安心</p>
+                                            <div class="title">
+                                                <picture class="icon-sp">
+                                                    <source srcset="/wp-content/uploads/business_icon08_pc.png">
+                                                    <img decoding="async" class="sizes" src="/wp-content/uploads/business_icon08_pc.png" alt="">
+                                                </picture>
+                                                <div class="sub-text">面倒な手続きもサポートで安心</div>
+                                            </div>
                                             <p class="text">経験豊富な電気代削減コンサルタントが<br/>しっかりサポートします。</p>
                                         </div>
                                     </li>
@@ -348,22 +413,24 @@
                             </picture>
                         </div>
                         <div class="text-content">
-                            <div class="point-title">
-                                <p class="ttl-en">
-                                    <span class="t1">POINT</span>
-                                    <span class="t2">02</span>
-                                </p>
-                                <p class="ttl">組合員のための<br/>消耗品等の共同購買</p>
-                            </div>
-                            <div class="text-description">
-                                <p class="text">組合員企業様のコスト削減に向けて、<br/>
-                                    多数の消耗品業者と提携することにより、<br/>
-                                    組合員の皆様へ特別価格で提供いたします。
-                                </p>
-                                <p class="text">各資材、事務用品等ご用意しておりますので、<br/>
-                                    お気軽にお問い合わせください。<br/>
-                                    また、共同購入商品やサービスのご希望等がございましたら、<br/>
-                                    ぜひご提案もお待ちしております。</p>
+                            <div class="text-inner">
+                                <div class="point-title">
+                                    <p class="ttl-en">
+                                        <span class="t1">POINT</span>
+                                        <span class="t2">02</span>
+                                    </p>
+                                    <p class="ttl">組合員のための<br/>消耗品等の共同購買</p>
+                                </div>
+                                <div class="text-description">
+                                    <p class="text">組合員企業様のコスト削減に向けて、<br/>
+                                        多数の消耗品業者と提携することにより、<br/>
+                                        組合員の皆様へ特別価格で提供いたします。
+                                    </p>
+                                    <p class="text">各資材、事務用品等ご用意しておりますので、<br/>
+                                        お気軽にお問い合わせください。<br/>
+                                        また、共同購入商品やサービスのご希望等がございましたら、<br/>
+                                        ぜひご提案もお待ちしております。</p>
+                                </div>
                             </div>
                         </div>
                     </div>
